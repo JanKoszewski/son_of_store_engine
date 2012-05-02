ace907475ac1e48db1a53aba0a72f5afa6f4466c
Jan Koszewski <jan.koszewski@gmail.com>
2012-05-02 11:04:18 -0400

    pared down seed file to just users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,3 @@
-require 'fabrication'
-
-stores =  10.times.map { |n| Fabricate(:store, :name => "Seed Store #{n}", :slug => "seed-store-#{n}") }
-100.times { Fabricate(:product, :photo => '', :store => Store.all.sample) }
-10.times { Fabricate(:category) }
-
-Product.all.each do |product|
-  c = Category.all.sample
-  c.store = product.store
-  c.save()
-  c.add_product(product)
-end
-
-20.times do
-  Product.all.sample.add_category(Category.all.sample)
-end
-
-["pending", "cancelled", "paid", "shipped", "returned"].each do |status|
-  3.times { Fabricate(:order, :status => status) }
-end
-
 u1 = User.create(
   :name => 'Matt Yoho',
   :email => 'demo10+matt@jumpstartlab.com',
@@ -38,12 +17,3 @@ User.create(
   :password => 'hungry',
   :username => 'SaxPlayer'
 ).add_role(Role.super_admin)
-
-
-stores[0..4].each do |s|
-  s.add_admin(u1)
-end
-
-stores[4..-1].each do |s|
-  s.add_admin(u2)
-end
